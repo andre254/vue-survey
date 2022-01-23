b619d4b0c1ea3d24ba8979c7d820e6250118a4df
andre254 <andyangute@gmail.com>
2022-01-23 17:05:54 +0400

    layout and routing

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -4,7 +4,12 @@ import { createStore } from 'vuex'
 // store acceps the following configuration objects SGAMM.
 // after, head over to main.js and import the store.js
 const store = createStore({
-  state: {},
+  state: {
+    user: {
+      data: {},
+      token: null
+    }
+  },
   getters: {},
   actions: {},
   mutations: {},
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,30 +1,5 @@
-<!--
-  This example requires Tailwind CSS v2.0+
 
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-gray-50">
-    <body class="h-full">
-    ```
-  -->
-  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div class="max-w-md w-full space-y-8">
       <div>
         <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
@@ -33,17 +8,21 @@
         <p class="mt-2 text-center text-sm text-gray-600">
           Or
           {{ ' ' }}
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-            start your 14-day free trial
-          </a>
+          <router-link :to="{name: 'Login'}" class="font-medium text-indigo-600 hover:text-indigo-500">
+            Login to your account
+          </router-link>
         </p>
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST">
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
+            <label for="full-name" class="sr-only">Name</label>
+            <input id="full-name" name="name" type="name" autocomplete="name" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Name" />
+          </div>
+          <div>
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
@@ -71,12 +50,10 @@
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
             </span>
-            Sign in
+            Register
           </button>
         </div>
       </form>
-    </div>
-  </div>
 </template>
 
 <script>
