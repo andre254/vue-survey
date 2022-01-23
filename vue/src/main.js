d51bc17d9c989e4a19965218a7cbe50748a86577
andre254 <andyangute@gmail.com>
2022-01-23 13:05:41 +0400

    setting up project

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import store from './store'
+import router from './router'
+// this is for tailwind styles
+import './index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.use(store)
+.mount('#app')
---
+++ vue/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+
+// store acceps the following configuration objects SGAMM.
+// after, head over to main.js and import the store.js
+const store = createStore({
+  state: {},
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {},
+
+})
+
+export default store;
